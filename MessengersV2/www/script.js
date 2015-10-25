e4be162ff3214833e1d58f5a0546509fac438ce7
Boaz Yaari <boazy@one1.co.il>
2015-10-25 13:36:09 +0200

    first commit

---
+++ MessengersV2/www/script.js
@@ -0,0 +1,36 @@
+﻿var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+// configure our routes
+scotchApp.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl: 'pages/login.html',
+            controller: 'loginController'
+        })
+
+        // route for the about page
+        .when('/resetPass', {
+            templateUrl: 'pages/resetPass.html',
+            controller: 'forgetPassController'
+        })
+     .when('/deliver', {
+         templateUrl: 'pages/deliver.html',
+         controller: 'deliverController'
+     })
+     .when('/collect', {
+         templateUrl: 'pages/collect.html',
+         controller: 'collectController'
+     })
+     .when('/collect_joined', {
+         templateUrl: 'pages/collect_joined.html',
+         controller: 'collect_joinedController'
+     })
+
+
+
+    
+    ;
+
+});
